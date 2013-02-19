6dc62748b20de78e451a44543e38301ed0afb9c6
Chris Hansen <chrisbuehansen@gmail.com>
2013-02-19 12:27:19 +0100

    DAL BeginsWith

--- WordSearch/src/DAL/WordSearchDBManager.java
+++ WordSearch/src/DAL/WordSearchDBManager.java
@@ -14,16 +14,33 @@ import java.util.ArrayList;
  */
 public class WordSearchDBManager
 {
+
     public ArrayList getList() throws Exception
     {
-       BufferedReader br = new BufferedReader(new FileReader("brit-a-z.txt"));
-       ArrayList list = new ArrayList();
-       while(br.ready())
-       {
-           String word = br.readLine();  
-           list.add(word);
-       }
-       br.close();
-       return list;
+        BufferedReader br = new BufferedReader(new FileReader("brit-a-z.txt"));
+        ArrayList list = new ArrayList();
+        while (br.ready())
+        {
+            String word = br.readLine();
+            list.add(word);
+        }
+        br.close();
+        return list;
+    }
+
+    public ArrayList beginsWith(String letters) throws Exception
+    {
+        BufferedReader br = new BufferedReader(new FileReader("brit-a-z.txt"));
+        ArrayList list = new ArrayList();
+        while (br.ready())
+        {
+            String word = br.readLine();
+            if (word.startsWith(letters))
+            {
+                list.add(word);
+            }
+        }
+        br.close();
+        return list;
     }
 }
