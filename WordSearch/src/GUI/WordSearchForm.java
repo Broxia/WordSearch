fbce2c40e0994fb681caae6da384630c3e3fcee3
Chris Hansen <chrisbuehansen@gmail.com>
2013-02-19 09:57:21 +0100

    First version of the GUI

--- WordSearch/src/GUI/WordSearchForm.java
+++ WordSearch/src/GUI/WordSearchForm.java
@@ -17,6 +17,7 @@ public class WordSearchForm extends javax.swing.JFrame
     public WordSearchForm()
     {
         initComponents();
+        setTitle(null);
     }
 
     /**
@@ -29,21 +30,224 @@ public class WordSearchForm extends javax.swing.JFrame
     private void initComponents()
     {
 
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        lblQuery = new javax.swing.JLabel();
+        txtSearch = new javax.swing.JTextField();
+        btnClear = new javax.swing.JButton();
+        btnSearch = new javax.swing.JButton();
+        lblResult = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        lstResults = new javax.swing.JList();
+        pnlSearchType = new javax.swing.JPanel();
+        rbtnBeginsWith = new javax.swing.JRadioButton();
+        rbtnContains = new javax.swing.JRadioButton();
+        rbtnEndswith = new javax.swing.JRadioButton();
+        rbtnExact = new javax.swing.JRadioButton();
+        lblSearchType = new javax.swing.JLabel();
+        pnlStyle = new javax.swing.JPanel();
+        lblStyle = new javax.swing.JLabel();
+        chkbxCaseSensitive = new javax.swing.JCheckBox();
+        pnlLimitation = new javax.swing.JPanel();
+        lblLimitation = new javax.swing.JLabel();
+        cmbbxLimitation = new javax.swing.JComboBox();
+        btnClose = new javax.swing.JButton();
+        lblCount = new javax.swing.JLabel();
+        lblCountDone = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("WordSearch");
+
+        lblQuery.setText("Query:");
+
+        btnClear.setText("Clear");
+
+        btnSearch.setText("Search");
+
+        lblResult.setText("Result:");
+
+        jScrollPane1.setViewportView(lstResults);
+
+        pnlSearchType.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        pnlSearchType.setName(""); // NOI18N
+
+        rbtnBeginsWith.setText("Begins with");
+
+        rbtnContains.setText("Contains");
+
+        rbtnEndswith.setText("Ends with");
+
+        rbtnExact.setText("Exact");
+
+        lblSearchType.setText("Search type");
+
+        org.jdesktop.layout.GroupLayout pnlSearchTypeLayout = new org.jdesktop.layout.GroupLayout(pnlSearchType);
+        pnlSearchType.setLayout(pnlSearchTypeLayout);
+        pnlSearchTypeLayout.setHorizontalGroup(
+            pnlSearchTypeLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(pnlSearchTypeLayout.createSequentialGroup()
+                .add(pnlSearchTypeLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(pnlSearchTypeLayout.createSequentialGroup()
+                        .add(20, 20, 20)
+                        .add(pnlSearchTypeLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                            .add(rbtnBeginsWith)
+                            .add(rbtnContains)
+                            .add(rbtnEndswith)
+                            .add(rbtnExact)))
+                    .add(pnlSearchTypeLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .add(lblSearchType)))
+                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        pnlSearchTypeLayout.setVerticalGroup(
+            pnlSearchTypeLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(org.jdesktop.layout.GroupLayout.TRAILING, pnlSearchTypeLayout.createSequentialGroup()
+                .add(lblSearchType)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 10, Short.MAX_VALUE)
+                .add(rbtnBeginsWith)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(rbtnContains)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(rbtnEndswith)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(rbtnExact)
+                .addContainerGap())
+        );
+
+        pnlStyle.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        lblStyle.setText("Style");
+
+        chkbxCaseSensitive.setText("Case sensitive");
+        chkbxCaseSensitive.addActionListener(new java.awt.event.ActionListener()
+        {
+            public void actionPerformed(java.awt.event.ActionEvent evt)
+            {
+                chkbxCaseSensitiveActionPerformed(evt);
+            }
+        });
+
+        org.jdesktop.layout.GroupLayout pnlStyleLayout = new org.jdesktop.layout.GroupLayout(pnlStyle);
+        pnlStyle.setLayout(pnlStyleLayout);
+        pnlStyleLayout.setHorizontalGroup(
+            pnlStyleLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(pnlStyleLayout.createSequentialGroup()
+                .addContainerGap()
+                .add(pnlStyleLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(pnlStyleLayout.createSequentialGroup()
+                        .add(10, 10, 10)
+                        .add(chkbxCaseSensitive))
+                    .add(lblStyle))
+                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        pnlStyleLayout.setVerticalGroup(
+            pnlStyleLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(pnlStyleLayout.createSequentialGroup()
+                .add(lblStyle)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.UNRELATED)
+                .add(chkbxCaseSensitive)
+                .add(0, 25, Short.MAX_VALUE))
+        );
+
+        pnlLimitation.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        lblLimitation.setText("Limitation");
+
+        org.jdesktop.layout.GroupLayout pnlLimitationLayout = new org.jdesktop.layout.GroupLayout(pnlLimitation);
+        pnlLimitation.setLayout(pnlLimitationLayout);
+        pnlLimitationLayout.setHorizontalGroup(
+            pnlLimitationLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(pnlLimitationLayout.createSequentialGroup()
+                .addContainerGap()
+                .add(pnlLimitationLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(pnlLimitationLayout.createSequentialGroup()
+                        .add(10, 10, 10)
+                        .add(cmbbxLimitation, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 80, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+                    .add(lblLimitation))
+                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        pnlLimitationLayout.setVerticalGroup(
+            pnlLimitationLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(pnlLimitationLayout.createSequentialGroup()
+                .add(lblLimitation)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.UNRELATED)
+                .add(cmbbxLimitation, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .add(0, 26, Short.MAX_VALUE))
+        );
+
+        btnClose.setText("Close");
+
+        lblCount.setText("Count:");
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(0, 400, Short.MAX_VALUE)
+            .add(layout.createSequentialGroup()
+                .addContainerGap()
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(lblQuery)
+                    .add(lblResult))
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 8, Short.MAX_VALUE)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING, false)
+                    .add(layout.createSequentialGroup()
+                        .add(lblCount)
+                        .add(18, 18, 18)
+                        .add(lblCountDone, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 45, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 161, Short.MAX_VALUE)
+                        .add(btnClose))
+                    .add(layout.createSequentialGroup()
+                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING, false)
+                            .add(jScrollPane1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                            .add(txtSearch, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 164, Short.MAX_VALUE))
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING, false)
+                            .add(pnlLimitation, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
+                                .add(btnSearch)
+                                .add(18, 18, 18)
+                                .add(btnClear))
+                            .add(pnlSearchType, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .add(pnlStyle, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                .addContainerGap(23, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(0, 300, Short.MAX_VALUE)
+            .add(layout.createSequentialGroup()
+                .add(23, 23, 23)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(lblQuery)
+                    .add(btnClear)
+                    .add(btnSearch)
+                    .add(txtSearch, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+                .add(30, 30, 30)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING, false)
+                    .add(lblResult)
+                    .add(layout.createSequentialGroup()
+                        .add(pnlSearchType, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                        .add(pnlStyle, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                        .add(pnlLimitation, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+                    .add(jScrollPane1))
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(layout.createSequentialGroup()
+                        .add(9, 9, 9)
+                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                            .add(lblCount)
+                            .add(lblCountDone, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 16, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)))
+                    .add(layout.createSequentialGroup()
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                        .add(btnClose)))
+                .addContainerGap(24, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void chkbxCaseSensitiveActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_chkbxCaseSensitiveActionPerformed
+    {//GEN-HEADEREND:event_chkbxCaseSensitiveActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_chkbxCaseSensitiveActionPerformed
 
     /**
      * @param args the command line arguments
@@ -94,5 +298,28 @@ public class WordSearchForm extends javax.swing.JFrame
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnClear;
+    private javax.swing.JButton btnClose;
+    private javax.swing.JButton btnSearch;
+    private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JCheckBox chkbxCaseSensitive;
+    private javax.swing.JComboBox cmbbxLimitation;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lblCount;
+    private javax.swing.JLabel lblCountDone;
+    private javax.swing.JLabel lblLimitation;
+    private javax.swing.JLabel lblQuery;
+    private javax.swing.JLabel lblResult;
+    private javax.swing.JLabel lblSearchType;
+    private javax.swing.JLabel lblStyle;
+    private javax.swing.JList lstResults;
+    private javax.swing.JPanel pnlLimitation;
+    private javax.swing.JPanel pnlSearchType;
+    private javax.swing.JPanel pnlStyle;
+    private javax.swing.JRadioButton rbtnBeginsWith;
+    private javax.swing.JRadioButton rbtnContains;
+    private javax.swing.JRadioButton rbtnEndswith;
+    private javax.swing.JRadioButton rbtnExact;
+    private javax.swing.JTextField txtSearch;
     // End of variables declaration//GEN-END:variables
 }
