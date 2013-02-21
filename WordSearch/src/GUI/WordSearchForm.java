f980688118824596d649e7b4c59d5148e872d3aa
Dennis List <d.listedreng@hotmail.com>
2013-02-21 11:25:11 +0100

    Oprydning

--- WordSearch/src/GUI/WordSearchForm.java
+++ WordSearch/src/GUI/WordSearchForm.java
@@ -323,7 +323,7 @@ public class WordSearchForm extends javax.swing.JFrame
         });
     }
 
-    public void listAll()
+    private void listAll()
     {
         try
         {
@@ -338,7 +338,7 @@ public class WordSearchForm extends javax.swing.JFrame
         }
     }
 
-    public void counter()
+    private void counter()
     {
         int i = listModel.getSize();
         String nr = Integer.toString(i);
